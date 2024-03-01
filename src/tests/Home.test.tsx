c6e1f28c5d263b38dc55598d8669636bf1e5a729
Francis Lagares <francis.lagares@gmail.com>
2024-03-01 19:24:47 +0100

    feat: main page unloaded

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,14 +1,14 @@
-import { render, screen, within } from '@testing-library/react';
-import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { expect } from 'vitest';
 
 import Home from '@/app/page';
 
-test('Pages Router', () => {
-  render(<Home />);
+describe('Home Page', () => {
+  it('renders without crashing', () => {
+    render(<Home />);
 
-  const main = within(screen.getByRole('main'));
+    const main = screen.getByRole('main');
 
-  expect(
-    main.getByRole('link', { name: /By Vercel Logo/i }),
-  ).toBeInTheDocument();
+    expect(main).toBeInTheDocument();
+  });
 });
