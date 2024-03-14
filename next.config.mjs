a94a4ad5046c38480673fe344535781ae0bc7bd9
Francis Lagares <francis.lagares@gmail.com>
2024-03-14 09:59:45 +0100

    fix: environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
-import { fileURLToPath } from 'node:url';
-
-import createJiti from 'jiti';
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-// Import env here to validate during build. Using jiti we can import .ts files :)
-jiti('./src/env.ts');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
